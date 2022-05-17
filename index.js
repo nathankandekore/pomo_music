0ca638d2fbd6644da2f53f732ae960bf76435441
Nathan Kandekore-White <nathankandekore-white@Nathans-MacBook-Air.local>
2022-05-17 23:37:27 +0100

    changes to start and reset

--- index.js
+++ index.js
@@ -1,47 +1,72 @@
 // timer functionsallity for buttons
 // fetch Api from spotify
 const message = document.getElementById("message");
+let clock = document.getElementById("stopwatch");
+let seconds = 1500;
+let timeout;
+
+// START/STOP TIMER
+
+function startTimer() {
+  timeout = setInterval(() => {
+    seconds--;
+    console.log(seconds);
+    displayTimer();
+  }, 1000);
+}
+
+function resetTimer() {
+  seconds == 0;
+}
+
+// DISPLAY
+
+function displayTimer() {
+  clock.innerText = `${Math.floor(seconds / 60)} : ${seconds % 60}`;
+}
+
+// BUTTONS
+
 const shortBtn = document
   .getElementById("short-break-btn")
   .addEventListener("click", () => {
     seconds = 300;
-    clock.innerText = `${Math.floor(seconds / 60)} : ${seconds % 60}`;
+    displayTimer();
   });
+
 const longBtn = document
   .getElementById("long-break-btn")
   .addEventListener("click", () => {
     seconds = 900;
     seconds = 900;
-    clock.innerText = `${Math.floor(seconds / 60)} : ${seconds % 60}`;
+    displayTimer();
   });
+
 const pomodoroBtn = document
   .getElementById("pomodoro-break-btn")
   .addEventListener("click", () => {
     seconds = 1500;
-    clock.innerText = `${Math.floor(seconds / 60)} : ${seconds % 60}`;
+    displayTimer();
   });
+
 const startBtn = document
   .getElementById("start-btn")
   .addEventListener("click", () => {
     console.log("start clicked");
+    startTimer();
   });
+
 const resetBtn = document
   .getElementById("reset-btn")
   .addEventListener("click", () => {
     console.log("reset clicked");
-    seconds = 1500;
-    testFunc();
+    // how do you go back to last timer?
+    clearInterval(timeout);
+    seconds == 0;
+    displayTimer();
   });
-let clock = document.getElementById("stopwatch");
 
-let seconds = 2400;
-
-// DISPLAY
-const testFunc = function () {
-  clock.innerText = `${Math.floor(seconds / 60)} : ${seconds % 60}`;
-};
-
-// testFunc();
+// set state by initializing and adjusting variable eg pom, short, or long
 
 // COUNTDOWN
 // const decrement = function () {
@@ -53,29 +78,6 @@ const testFunc = function () {
 //   }
 // };
 
-// BREAKS FUNCTIONS
-
-// BUTTONS FUNCTIONS
-
-// const startBtn = document
-//   .getElementById("start-btn")
-//   .addEventListener("click", () => {
-//     decrement();
-//   });
-
-// const resetBtn = document
-//   .getElementById("reset-btn")
-//   .addEventListener("click", () => {
-//     // reset();
-//   });
-
 // NEW REST FUNCTION
 // AMMEND ALL THE BUTTONS
 // MUSIC PLAYER FUNCTIONALITY
-
-// const reset = function () {
-//   clearInterval();
-//   let min = 25;
-//   let sec = 00;
-//   clock.innerHTML = `${min}:${sec}`;
-// };
